7d57b91c5da4b1bc0050756e996a07d06a48b5cb
ClementChooJunKai <clementchoo78@gmail.com>
2023-07-02 23:38:24 +0800

    register(mysql)

--- model/__init__.py
+++ model/__init__.py
@@ -1,3 +1,4 @@
+import random
 from flask_mysqldb import MySQL
 from App import app
 from flask import Flask, session, redirect, url_for, request
@@ -53,7 +54,6 @@ def checkusername():
 
 def registerUser():
     # Establish a connection to MySQL
-   
     mysql_cursor = mysql.connection.cursor()
 
     fields = [k for k in request.form]
@@ -65,11 +65,29 @@ def registerUser():
     user_data["created_at"] = datetime.now()
     db.user.insert_one(user_data)
 
+    checkstoreid_query = "SELECT * FROM store WHERE storename = %s"
+    mysql_cursor.execute(checkstoreid_query, (user_data["username"],))
+    owndata = mysql_cursor.fetchall()
+    maxstoreid_query = "SELECT max(storeid) FROM store"
+    mysql_cursor.execute(maxstoreid_query)
+    mstoreid = mysql_cursor.fetchall()
+    print(mstoreid)
+    maxstoreid = mstoreid[0][0] + 1
+    if owndata:
+        # Insert the owndata into the Account table
+        account_query = "INSERT INTO Account (username, email, fullName, password, hashedPassword, storeid) VALUES (%s, %s, %s, %s, %s, %s)"
+        account_values = (user_data["username"], user_data["email"], user_data["name"], user_data["password"], user_data["confirmpassword"], int(owndata[0][0]))
+        mysql_cursor.execute(account_query, account_values)
+    else:
+        # Insert 0 as the owndata
+        storedetail_query = "INSERT INTO Store (storeid, storeName, storeSlug, storeRating, storeAmtRating, productCount, storeFollowers, storeJoinedDate, chatPerformance, replyRate, platformType) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        store_values = (maxstoreid, user_data["username"], 'nil', 0, 0, 0, 0, '1 day ago', 0, 'within a few minutes', 'nil')
+        mysql_cursor.execute(storedetail_query, store_values)
+        account_query = "INSERT INTO Account (username, email, fullName, password, hashedPassword, storeId) VALUES (%s, %s, %s, %s, %s, %s)"
+        account_values = (user_data["username"], user_data["email"], user_data["name"], user_data["password"], user_data["confirmpassword"], maxstoreid)
+        mysql_cursor.execute(account_query, account_values)
+      
 
-    account_query = "INSERT INTO Account ( username, email, fullName, password,hashedPassword) VALUES (%s, %s, %s, %s,%s)"
-    account_values = ( user_data["username"], user_data["email"], user_data["name"],user_data["password"], user_data["confirmpassword"])
-    mysql_cursor.execute(account_query, account_values)
-    
     mysql_cursor.close()
     mysql.connection.commit()
     mysql.connection.close()
