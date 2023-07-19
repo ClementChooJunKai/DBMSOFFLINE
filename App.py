b1b3854a11c971129d32d19423960748e0b2a2ca
ClementChooJunKai <clementchoo78@gmail.com>
2023-07-19 18:37:02 +0800

    abc

--- App.py
+++ App.py
@@ -1,6 +1,8 @@
 from collections import Counter
 import decimal
 from itertools import count
+
+from flask.json import jsonify
 from helpers.database import *
 import nltk
 from nltk.tokenize import word_tokenize
@@ -8,6 +10,11 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 from model import *
 import re
+from flask_mail import Mail, Message
+
+
+
+
 app = Flask(__name__)
 app.secret_key = "ITSASECRET"
 
@@ -18,6 +25,12 @@ app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'dbms'
 
 # Create MySQL instance
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'tay.jiale@gmail.com'
+app.config['MAIL_PASSWORD'] = 'tfclioagsaftodjo'
+mail = Mail(app)
 mysql = MySQL(app)
 
 
@@ -606,6 +619,94 @@ def update_product():
     # Redirect to a success page or perform any other necessary action
     return redirect('/success')
 
+@app.route('/getCatP')
+def getcat():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT category FROM product p INNER JOIN store s ON p.storeId = s.storeId WHERE s.storeName = %s", (session['username'],))
+    fetchdata = cur.fetchall()
+    cur.close()
+
+    category_dict = {}
+
+    for row in fetchdata:
+        category_string = row[0]
+        category_values = category_string.split(';')
+
+        main_category = category_values[2]
+        subcategories = category_values[3:]
+
+        main_category_dict = category_dict.setdefault(main_category, set())
+
+        for subcategory in subcategories:
+            if ':' in subcategory:
+                break  # Stop processing subcategories when a tuple is encountered
+
+            main_category_dict.add(subcategory)
+
+    main_categories = list(category_dict.keys())  # Get the main categories
+    sub_categories = {main_category: list(subs) for main_category, subs in category_dict.items()}  # Convert subcategories to dictionary format
+
+    return render_template('CatProducts.html', main_categories=main_categories, sub_categories=sub_categories)
+
+
+from decimal import Decimal
+import json
+
+@app.route('/get-product-cat', methods=["GET"])
+def get_product_cat():
+    main_category = request.args.get('main_category')
+    sub_categories = request.args.getlist('sub_categories')
+
+    cur = mysql.connection.cursor()
+
+    # Construct the SQL query based on the selected main category and subcategories
+    query = """
+        SELECT *
+        FROM product
+        WHERE category LIKE %s
+    """
+    params = [f'%{main_category}%']
+
+    if sub_categories:
+        query += "or ("
+        for i in range(len(sub_categories)):
+            query += "category LIKE %s"
+            params.append(f'%{sub_categories[i]}%')
+            if i < len(sub_categories) - 1:
+                query += " OR "
+        query += ")"
+
+    cur.execute(query, params)
+    print(query, params)
+
+    fetchdata = cur.fetchall()
+
+    cur.close()
+
+    # Convert Decimal objects to string representations
+    serialized_data = []
+    for row in fetchdata:
+        serialized_row = [str(item) if isinstance(item, Decimal) else item for item in row]
+        serialized_data.append(serialized_row)
+
+    # Return JSON response
+    return json.dumps(serialized_data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # Redirect to a success page or perform any other necessary action
+    
 
 @app.route('/success')
 def success():
@@ -625,6 +726,25 @@ def login_page():
         return render_template('login.html')
 
 # Register new user
+@app.route('/generateotp', methods=['GET','POST'])
+def generateotp():
+        if request.method == 'POST':
+            otp_username = request.form.get('username')
+            print(otp_username)
+            check = db.user.find_one({"username":  otp_username})
+
+                # Generate a random 6-digit password
+            otp = str(random.randint(100000, 999999))
+
+            # Send the 6-digit password to the user's email
+            msg = Message("2FA Verification Code", sender="your-email@example.com", recipients=[check["email"]])
+            msg.body = f"Your verification code is: {otp}"
+            mail.send(msg)
+            # Store the generated OTP in the session
+            session["otp"] = otp
+            return redirect(url_for('checkloginpassword'))
+        else:
+            return render_template('login.html') # Render the login form
 
 
 @app.route('/register', methods=["GET", "POST"])
@@ -652,6 +772,9 @@ def login():
             return render_template("login.html")
         else:
             return redirect(url_for("home"))
+
+
+
 
 # Check if username exists
 
--- static/js/checkLoginusername.js
+++ static/js/checkLoginusername.js
@@ -3,23 +3,27 @@ $(document).ready(function(){
         var username = $('#username').val();
 
         if(username != ''){
-  
-           $.ajax({
-              url: '/checkloginusername',
-              type: 'post',
-              data: {username: username},
-              success: function(response){
+            // Save the username in the session
+            sessionStorage.setItem('tempusername', username);
+
+            $.ajax({
+                url: '/checkloginusername',
+                type: 'post',
+                data: {username: username},
+                success: function(response){
                     if (response == "No User"){
                         $('#uname_response').html("User does not exist").css({'color':'red', 'text-align':'right'});
                         $('#button').prop('disabled', true);
-                    }else{
+                    } else {
                         $('#uname_response').html("");
                         $('#button').removeAttr('disabled');
                     }
-               }
-           });
-        }else{
-           $("#uname_response").html("");
+                }
+            });
+        } else {
+            $("#uname_response").html("");
         }
-    })
-})
+    });
+
+    
+});
