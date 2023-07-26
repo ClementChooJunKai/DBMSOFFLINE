bce44003cae7d3e81f3d9faf7881b073c6e7182d
Darren <darrenleecw77@gmail.com>
2023-07-26 14:46:00 +0800

    ai name generator bot

--- pages/products.py
+++ pages/products.py
@@ -1,28 +1,36 @@
-
 import traceback
 from flask import Flask, Blueprint, render_template, request, redirect, url_for, session
 from model import *
 from utils import *
 
 from decimal import Decimal
+from langchain.agents import create_csv_agent
 
-products_blueprint = Blueprint('products', __name__)
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.agents.agent_types import AgentType
+from dotenv import load_dotenv
+from flask import jsonify
+
+load_dotenv()
+
+products_blueprint = Blueprint("products", __name__)
 
 
 # Products Page
-@products_blueprint.route('/products', methods=["GET"])
+@products_blueprint.route("/products", methods=["GET"])
 def products_page():
-    if 'username' in session:
-        username = session['username']
+    if "username" in session:
+        username = session["username"]
 
         cur = mysql.connection.cursor()
-        #original query
+        # original query
         # query = "SELECT p.productid, p.ProductName, p.Productdesc, p.sellingprice, p.discountedprice, p.category, p.quantitysold, p.productlikes, p.productratings, p.productratingsamt, p.shippingtype, p.shipfrom FROM product p INNER JOIN store s ON p.storeid = s.storeid WHERE s.storename = %s;"
-        
+
         # v1 query
         query = "SELECT p.productid, p.ProductName, p.Productdesc, p.sellingprice, p.discountedprice, p.category, p.quantitysold, p.productlikes, p.productratings, p.productratingsamt, p.shippingtype, p.shipfrom, w.watchlistId FROM product p INNER JOIN store s ON p.storeid = s.storeid LEFT JOIN watchlist w ON p.productid = w.watched_id WHERE s.storename = %s;"
-        
-        #v2 query
+
+        # v2 query
         # query = """
         #     SELECT p.productid, p.ProductName, p.Productdesc, p.sellingprice, p.discountedprice, p.category,
         #            p.quantitysold, p.productlikes, p.productratings, p.productratingsamt,
@@ -32,53 +40,64 @@ def products_page():
         #     LEFT JOIN watchlist w ON p.productid = w.watched_id
         #     WHERE s.storename = %s;
         # """
-        
+
         cur.execute(query, (username,))
         fetchdata = cur.fetchall()
-        
-        #old
+
+        # old
         # stripped_data = [[str(item).strip() for item in row]
         #                 for row in fetchdata]
 
-        #new
-        stripped_data = [[str(item).strip() if item is not None else None for item in row]
-                         for row in fetchdata]
+        # new
+        stripped_data = [
+            [str(item).strip() if item is not None else None for item in row]
+            for row in fetchdata
+        ]
 
         cur.close()
-        return render_template("products/products.html", data=stripped_data, username=username)
+        return render_template(
+            "products/products.html", data=stripped_data, username=username
+        )
     else:
         return "User not logged in"
-    
-    
 
 
-@products_blueprint.route('/editProduct/<int:product_id>', methods=['GET'])
+@products_blueprint.route("/editProduct/<int:product_id>", methods=["GET"])
 def view_store(product_id):
     cur = mysql.connection.cursor()
-    cur.execute("SELECT p.productid,p.ProductName,p.Productdesc,p.sellingprice,p.discountedprice,p.category,p.quantitysold,p.productlikes,p.productratings,p.productratingsamt,p.shippingtype,p.shipfrom FROM product p INNER JOIN store s ON p.storeid = s.storeid WHERE p.productid = %s;", (product_id,))
+    cur.execute(
+        "SELECT p.productid,p.ProductName,p.Productdesc,p.sellingprice,p.discountedprice,p.category,p.quantitysold,p.productlikes,p.productratings,p.productratingsamt,p.shippingtype,p.shipfrom FROM product p INNER JOIN store s ON p.storeid = s.storeid WHERE p.productid = %s;",
+        (product_id,),
+    )
     fetchdata = cur.fetchall()
 
     cur.close()
     return render_template("products/editProduct.html", data=fetchdata)
 
 
-@products_blueprint.route('/optimizeProduct/<int:product_id>', methods=['GET'])
+@products_blueprint.route("/optimizeProduct/<int:product_id>", methods=["GET"])
 def optimize(product_id):
     cur = mysql.connection.cursor()
-    cur.execute("SELECT p.productid,p.ProductName,p.Productdesc,p.sellingprice,p.discountedprice,p.category,p.quantitysold,p.productlikes,p.productratings,p.productratingsamt,p.shippingtype,p.shipfrom FROM product p INNER JOIN store s ON p.storeid = s.storeid WHERE p.productid = %s;", (product_id,))
+    cur.execute(
+        "SELECT p.productid,p.ProductName,p.Productdesc,p.sellingprice,p.discountedprice,p.category,p.quantitysold,p.productlikes,p.productratings,p.productratingsamt,p.shippingtype,p.shipfrom FROM product p INNER JOIN store s ON p.storeid = s.storeid WHERE p.productid = %s;",
+        (product_id,),
+    )
     fetchdata = cur.fetchall()
     cur.execute(
-        "SELECT p.category FROM product p INNER JOIN store s ON p.storeid = s.storeid WHERE p.productid = %s;", (product_id,))
+        "SELECT p.category FROM product p INNER JOIN store s ON p.storeid = s.storeid WHERE p.productid = %s;",
+        (product_id,),
+    )
     category_data = cur.fetchall()
 
     categories = category_data[0][0].split(";")
 
-# Skip the first value of stripped_categories
-# Assume you have an array of stripped categories
+    # Skip the first value of stripped_categories
+    # Assume you have an array of stripped categories
     stripped_categories = [category.strip() for category in categories][3:]
 
     if stripped_categories:
-        cur.execute("""
+        cur.execute(
+            """
         SELECT p.sellingprice, p.discountedprice,p.productid
         FROM product p
         INNER JOIN (
@@ -91,7 +110,9 @@ def optimize(product_id):
         ) AS subquery ON p.productid = subquery.productid
         INNER JOIN store s ON p.storeid = s.storeid
         WHERE p.category LIKE %s;
-    """, ['%' + stripped_categories[0] + '%', '%' + stripped_categories[0] + '%'])
+    """,
+            ["%" + stripped_categories[0] + "%", "%" + stripped_categories[0] + "%"],
+        )
     pricing = cur.fetchall()
 
     if stripped_categories:
@@ -99,7 +120,8 @@ def optimize(product_id):
         average_ratings = []
         all_ratings = []
         for category in stripped_categories:
-            cur.execute("""
+            cur.execute(
+                """
                 SELECT p.productratings
                 FROM product p
                 INNER JOIN store s ON p.storeid = s.storeid
@@ -107,15 +129,21 @@ def optimize(product_id):
                 AND p.productratings BETWEEN 1 AND 5
                 ORDER BY p.productratings DESC
                 
-            """, ['%' + category + '%'])
+            """,
+                ["%" + category + "%"],
+            )
             ratings = cur.fetchall()
             all_ratings.extend([rating[0] for rating in ratings])
-            average_rating = sum([rating[0] for rating in ratings]) / \
-                len(ratings) if len(ratings) > 0 else 0
+            average_rating = (
+                sum([rating[0] for rating in ratings]) / len(ratings)
+                if len(ratings) > 0
+                else 0
+            )
             average_ratings.append(average_rating)
 
     # Fetch top-rated products and their product names
-    cur.execute("""
+    cur.execute(
+        """
         SELECT p.ProductName
         FROM product p
         INNER JOIN (
@@ -128,7 +156,9 @@ def optimize(product_id):
         ) AS subquery ON p.productid = subquery.productid
         INNER JOIN store s ON p.storeid = s.storeid
         WHERE p.category LIKE %s;
-    """, ['%' + stripped_categories[0] + '%', '%' + stripped_categories[0] + '%'])
+    """,
+        ["%" + stripped_categories[0] + "%", "%" + stripped_categories[0] + "%"],
+    )
     top_rated_products = cur.fetchall()
 
     cur.close()
@@ -140,7 +170,8 @@ def optimize(product_id):
 
     # Flatten the list of product keywords
     flattened_keywords = [
-        keyword for sublist in product_keywords for keyword in sublist]
+        keyword for sublist in product_keywords for keyword in sublist
+    ]
 
     # Count the frequency of each keyword
     keyword_counts = Counter(flattened_keywords)
@@ -149,15 +180,25 @@ def optimize(product_id):
     frequency_threshold = 2
 
     # Filter out keywords that don't meet the frequency threshold and exclude symbols/numbers
-    filtered_common_keywords = [(keyword, count) for keyword, count in keyword_counts.items(
-    ) if count >= frequency_threshold and re.match(r'^[a-zA-Z]+$', keyword)]
+    filtered_common_keywords = [
+        (keyword, count)
+        for keyword, count in keyword_counts.items()
+        if count >= frequency_threshold and re.match(r"^[a-zA-Z]+$", keyword)
+    ]
 
-    return render_template("products/optimizeProduct.html", data=fetchdata, price=pricing, keywords=filtered_common_keywords, ratingData=all_ratings, avgrating=round(average_rating, 2))
+    return render_template(
+        "products/optimizeProduct.html",
+        data=fetchdata,
+        price=pricing,
+        keywords=filtered_common_keywords,
+        ratingData=all_ratings,
+        avgrating=round(average_rating, 2),
+    )
 
 
-@products_blueprint.route('/delete_product', methods=['POST'])
+@products_blueprint.route("/delete_product", methods=["POST"])
 def delete_product():
-    product_id = request.form.get('id')
+    product_id = request.form.get("id")
     print(product_id)
     # Connect to MySQL
     conn = mysql.connection
@@ -168,20 +209,21 @@ def delete_product():
         query = "DELETE FROM product WHERE productid = %s"
         cursor.execute(query, (product_id,))
         conn.commit()
-        return redirect('/success')
+        return redirect("/success")
     except Exception as error:
         # Handle any errors that occur during the deletion
         print(f"Error deleting product: {error}")
-        return redirect('/404')
+        return redirect("/404")
 
     finally:
         # Close the cursor
         cursor.close()
 
-@products_blueprint.route('/addProduct', methods=['GET'])
+
+@products_blueprint.route("/addProduct", methods=["GET"])
 def add_product():
-    if 'username' in session:
-        username = session['username']
+    if "username" in session:
+        username = session["username"]
 
         cur = mysql.connection.cursor()
 
@@ -195,54 +237,63 @@ def add_product():
         cur.execute(category_query)
         category_data = cur.fetchall()
         # Remove parentheses and commas from category values
-        category = [re.sub(r'[\(\),]', '', item[0]) for item in category_data]
+        category = [re.sub(r"[\(\),]", "", item[0]) for item in category_data]
 
         cur.execute(shipfrom_query)
         shipfrom_data = cur.fetchall()
         # Remove parentheses and commas from shipfrom values
-        shipfrom = [re.sub(r'[\(\),]', '', item[0]) for item in shipfrom_data]
+        shipfrom = [re.sub(r"[\(\),]", "", item[0]) for item in shipfrom_data]
 
-        return render_template('products/addProduct.html', data=fetchdata, category=category, shipfrom=shipfrom)
-@products_blueprint.route('/addProductDB', methods=['POST'])
+        return render_template(
+            "products/addProduct.html",
+            data=fetchdata,
+            category=category,
+            shipfrom=shipfrom,
+        )
+
+
+@products_blueprint.route("/addProductDB", methods=["POST"])
 def add_productDB():
-    
-        # Extract data from the form
-        product_name = request.form['ProductName']
-        product_description = request.form['ProductDescription']
-        product_categories = request.form['ProductCategories']
-        selling_price = request.form['sellingPrice']
-        storeID = request.form['id']
-        discount_percentage = request.form['discountPercentage']
-        discounted_price = (float(selling_price)*(100-float(discount_percentage)))/100
-        quantity = request.form['Quantity']
-        free_shipping = request.form.get('freeShipping')  # It will be 'Free shipping' if checked or None if not checked
-        shipFrom = request.form['shipFrom']
-        # Extract hidden fields
-        product_slug = request.form['productSlug']
-        product_likes = request.form['productLikes']
-        product_rating = request.form['productrating']
-        product_ratings_amt = request.form['productratingsamt']
+    # Extract data from the form
+    product_name = request.form["ProductName"]
+    product_description = request.form["ProductDescription"]
+    product_categories = request.form["ProductCategories"]
+    selling_price = request.form["sellingPrice"]
+    storeID = request.form["id"]
+    discount_percentage = request.form["discountPercentage"]
+    discounted_price = (float(selling_price) * (100 - float(discount_percentage))) / 100
+    quantity = request.form["Quantity"]
+    free_shipping = request.form.get(
+        "freeShipping"
+    )  # It will be 'Free shipping' if checked or None if not checked
+    shipFrom = request.form["shipFrom"]
+    # Extract hidden fields
+    product_slug = request.form["productSlug"]
+    product_likes = request.form["productLikes"]
+    product_rating = request.form["productrating"]
+    product_ratings_amt = request.form["productratingsamt"]
 
-        # Create a new Product object with the extracted data
-        product = {
-            'ProductName': product_name,
-            'Productdesc': product_description,
-            'sellingprice': selling_price,
-            'discountedprice': discounted_price,
-            'category': product_categories,
-            'quantitysold': quantity,
-            'productlikes': product_likes,
-            'productratings': product_rating,
-            'productratingsamt': product_ratings_amt,
-            'shippingtype': 'Free shipping' if free_shipping else '',
-            'shipfrom': shipFrom,
-            'productSlug': product_slug,
-            'StoreId': storeID
-        }
+    # Create a new Product object with the extracted data
+    product = {
+        "ProductName": product_name,
+        "Productdesc": product_description,
+        "sellingprice": selling_price,
+        "discountedprice": discounted_price,
+        "category": product_categories,
+        "quantitysold": quantity,
+        "productlikes": product_likes,
+        "productratings": product_rating,
+        "productratingsamt": product_ratings_amt,
+        "shippingtype": "Free shipping" if free_shipping else "",
+        "shipfrom": shipFrom,
+        "productSlug": product_slug,
+        "StoreId": storeID,
+    }
 
-        # Execute the SQL query to insert the new product into the database
-        cur = mysql.connection.cursor()
-        cur.execute("""
+    # Execute the SQL query to insert the new product into the database
+    cur = mysql.connection.cursor()
+    cur.execute(
+        """
         INSERT INTO product (
             ProductName, Productdesc, sellingprice, discountedprice, category, quantitysold, 
             productlikes, productratings, productratingsamt, shippingtype, shipfrom, 
@@ -253,41 +304,77 @@ def add_productDB():
             %(quantitysold)s, %(productlikes)s, %(productratings)s, %(productratingsamt)s, 
             %(shippingtype)s, %(shipfrom)s, %(productSlug)s, %(StoreId)s
         )
-    """, product)
+    """,
+        product,
+    )
 
-        # Commit the changes to the database
-        mysql.connection.commit()
+    # Commit the changes to the database
+    mysql.connection.commit()
 
-        # Close the cursor
-        cur.close()
+    # Close the cursor
+    cur.close()
 
-        # Redirect the user to a success page or back to the add product page
-        # You can customize this URL to match your application's structure
-        return redirect(url_for('success'))  # Replace 'products.add_product_page' with the actual URL of the add product page
+    # Redirect the user to a success page or back to the add product page
+    # You can customize this URL to match your application's structure
+    return redirect(
+        url_for("success")
+    )  # Replace 'products.add_product_page' with the actual URL of the add product page
 
-   
 
-   
+@products_blueprint.route("/generate_listing", methods=["POST"])
+def generate_listing():
+    agent = create_csv_agent(
+        ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613"),
+        "/Users/darrenlee/DBMS/p.csv",
+        verbose=True,
+        agent_type=AgentType.OPENAI_FUNCTIONS,
+    )
+    product_categories = request.form["ProductCategories"]
+    product = request.form["aiInput"]
+    bot = "you are a product name generator bot, given the data, desciption of a product and category of product"
+    data = "the data is data of current listed products.it has total 14 columns but you only need productName(name of product),productDesc(product description),selling price(selling price of product),discounted price (price of product after discount) , category( category of product concatenated based on their sub category) , products sold( number of products sold), productslikes (number of users that like this product), product rating (overall rating of a product by past buyers max is 5/5) and productRatingsAmt( which is the number of users that rate this producct)"
+    analysis = "what constitutes a good product is one that has a high rating,high quanitity sold and high product ratings amt"
+    user_input = (
+        "based on the",
+        product,
+        "in",
+        product_categories,
+        " generate a good product name so that",
+        product,
+        " will be a good product.give me 3 suggestions of names and a short write up on keywords i should include in my product name also give me a suggested description",
+    )
+    output = bot, data, analysis, user_input
+    ai_output = agent.run(output)
+    print("output of agent:", ai_output)
+    return ai_output
 
-@products_blueprint.route('/update_product', methods=['POST'])
+
+@products_blueprint.route("/update_product", methods=["POST"])
 def update_product():
     # Retrieve the form data
-    id = request.form['id']
-    product_name = request.form['ProductName']
-    product_description = request.form['ProductDescription']
-    selling_price = decimal.Decimal(request.form['sellingPrice'])
+    id = request.form["id"]
+    product_name = request.form["ProductName"]
+    product_description = request.form["ProductDescription"]
+    selling_price = decimal.Decimal(request.form["sellingPrice"])
 
-    discountPercentage = decimal.Decimal(request.form['discountPercentage'])
+    discountPercentage = decimal.Decimal(request.form["discountPercentage"])
     print(discountPercentage)
-    discounted_price = (selling_price*(100-discountPercentage))/100
-    quantity = request.form['Quantity']
-    free_shipping = request.form.get('freeShipping')  # Checkbox value
+    discounted_price = (selling_price * (100 - discountPercentage)) / 100
+    quantity = request.form["Quantity"]
+    free_shipping = request.form.get("freeShipping")  # Checkbox value
 
     # Perform the update operation using the retrieved data and the ID
     cur = mysql.connection.cursor()
     sql = "UPDATE product SET productName = %s, productDesc = %s, sellingprice = %s, discountedprice = %s, quantitysold = %s, shippingtype = %s WHERE productId = %s"
-    params = (product_name, product_description, selling_price,
-              discounted_price, quantity, free_shipping, id)
+    params = (
+        product_name,
+        product_description,
+        selling_price,
+        discounted_price,
+        quantity,
+        free_shipping,
+        id,
+    )
 
     print("SQL Statement:", sql)
     print("Parameters:", params)
@@ -299,4 +386,4 @@ def update_product():
     cur.close()
 
     # Redirect to a success page or perform any other necessary action
-    return redirect('/success')
+    return redirect("/success")
